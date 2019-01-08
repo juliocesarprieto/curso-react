8167f6d407458fe797c2a1bb5d39b17097423618
Julio Velazquez <julio.velazquez@indt.org.br>
2019-01-08 17:43:10 -0400

    Configurando Router, Route e BrowserHistory

--- cdc-admin/src/index.js
+++ cdc-admin/src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Router, Route, browserHistory } from 'react-router';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    (<Router history={ browserHistory }>
+        <Route path="/" component={App}/>
+        <Route path="/autor"/>
+        <Route path="/"/>
+    </Router>),
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
