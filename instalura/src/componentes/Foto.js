6c447e4eca39b3d1705571ec632fa3799ab84440
Julio Velazquez <julio.velazquez@indt.org.br>
2019-01-16 11:29:58 -0400

    Ciclo de vida e navegação entre usuarios

--- instalura/src/componentes/Foto.js
+++ instalura/src/componentes/Foto.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router';
 
 class FotoAtualizacoes extends Component {
     render(){
@@ -22,7 +23,7 @@ class FotoInfo extends Component {
               <div className="foto-info-likes">
                 {
                   this.props.foto.likers.map(liker =>{
-                    return (<a key={liker.login} href="#">{liker.login}, </a>)
+                    return (<Link key={liker.login} href={`/timeline/${liker.login}`}>{liker.login},</Link>)
                   })
                 } 
                  curtiram
@@ -41,7 +42,7 @@ class FotoInfo extends Component {
                   this.props.foto.comentarios.map(comentario =>{
                     return (
                       <li className="comentario" key={comentario.id} >
-                        <a className="foto-info-autor">{comentario.login}</a>
+                        <Link to={`/timeline/${comentario.login}`} className="foto-info-autor">{comentario.login}</Link>
                           {comentario.texto}
                       </li>
                     );
@@ -60,11 +61,9 @@ class FotoHeader extends Component {
               <figure className="foto-usuario">
                 <img src={this.props.foto.urlPerfil} alt="foto do usuario"/>
                 <figcaption className="foto-usuario">
-                  <a href="#">
-                    {
-                      this.props.foto.loginUsuario
-                    }
-                  </a>  
+                  <Link to={`/timeline/${this.props.foto.loginUsuario}`}>
+                    {this.props.foto.loginUsuario}
+                  </Link> 
                 </figcaption>
               </figure>
               <time className="foto-data">{this.props.foto.horario}</time>
